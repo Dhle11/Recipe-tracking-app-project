dd43dffc435f6ecb983b75f916a86c23f477da66
Duyen Le <hdl611@hotmail.com>
2022-02-11 17:32:51 -0800

    add TableHead component

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TableHead from "./TableHead";
 
 // TODO: Display the list of recipes using the structure of table that is provided.
 // TODO: Create at least one additional component that is used by this component.
@@ -8,16 +9,7 @@ function RecipeList({ recipes, deleteRecipe, updateRecipe, editRecipe }) {
   return (
     <div className="recipe-list">
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Cuisine</th>
-            <th>Photo</th>
-            <th>Ingredients</th>
-            <th>Preparation</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
+        <TableHead/>
         <tbody>
           {recipes.map((recipe, index) => (
             <tr key={index} className="recipe">
